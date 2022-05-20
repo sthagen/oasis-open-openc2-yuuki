094e6a5f0a65eb987c1d72f6abb98df229ae3c4d
Jerome Czachor <jerome.czachor@hii-tsd.com>
2022-05-20 13:27:46 -0400

    partial commit, add minor cleanup & plugfest producer

--- examples/mqtt_consumer_plugfest.py
+++ examples/mqtt_consumer_plugfest.py
@@ -1,16 +1,8 @@
 """
 Example Implementation of an OpenC2 MQTT Consumer
 """
-import argparse
-
 from yuuki.transports import (
-    MqttTransport,
-    MqttConfig,
-    MQTTAuthorization,
-    MQTTAuthentication,
-    BrokerConfig,
-    Publication,
-    Subscription
+    MqttTransport, MqttConfig, MQTTAuthorization, MQTTAuthentication, BrokerConfig, Publication, Subscription
 )
 
 from yuuki import Consumer
@@ -20,13 +12,9 @@ from actuators.slpf import slpf
 
 consumer = Consumer(rate_limit=60, versions=['1.0'], actuators=[slpf, SBOM, database])
 
-parser = argparse.ArgumentParser(description="MQTT Consumer")
-parser.add_argument("--host", help="MQTT Broker host")
-parser.add_argument("--port", type=int, help="MQTT Broker port")
-args = parser.parse_args()
-
 host = '35.221.11.97'
 port = 1883
+topics = ['oc2/cmd', 'oc2/cmd/ap/slpf', 'oc2/cmd/ap/database', 'oc2/cmd/ap/sbom']
 
 mqtt_config = MqttConfig(
     broker=BrokerConfig(
@@ -37,31 +25,22 @@ mqtt_config = MqttConfig(
         authorization=MQTTAuthorization(
             enable=True,
             username='plug',
-            password='fest'),
+            password='fest'
+        ),
         authentication=MQTTAuthentication(
             enable=False,
             certfile=None,
             keyfile=None,
-            ca_certs=None)),
-    subscriptions=[
-        Subscription(
-            topic='oc2/cmd',
-            qos=1),
-        Subscription(
-            topic='oc2/cmd/ap/slpf',
-            qos=1),
-        Subscription(
-            topic='oc2/cmd/ap/database',
-            qos=1),
-        Subscription(
-            topic='oc2/cmd/ap/sbom',
-            qos=1)
-    ],
+            ca_certs=None
+        )
+    ),
+    subscriptions=[Subscription(topic=t, qos=1) for t in topics],
     publications=[
         Publication(
             topic='oc2/rsp',
             qos=1
-        )]
+        )
+    ]
 )
 
 mqtt_consumer = MqttTransport(consumer=consumer, config=mqtt_config)
--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
-import setuptools
+from pathlib import Path
+from pkg_resources import parse_requirements
+from setuptools import setup
 
-setuptools.setup()
+
+def get_requirements():
+    with Path('requirements.txt').open() as req:
+        return [str(req) for req in parse_requirements(req)]
+
+
+setup(
+    name='Yuuki',
+    package_data={
+        'Yuuki': ['./yuuki/*']
+    },
+    install_requires=get_requirements()
+)
