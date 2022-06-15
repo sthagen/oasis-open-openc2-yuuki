829e27d3212a3a5785aa6ec91bec742cfa10961c
kgcressman <kgcressman@praxiseng.com>
2022-06-15 14:56:50 +0000

    Update examples/producers/openc2_command.py

--- examples/producers/openc2_command.py
+++ examples/producers/openc2_command.py
@@ -3,27 +3,6 @@ import uuid
 
 
 
-query_database = {
-    "headers": {
-        "request_id": str(uuid.uuid4()),
-        "created": round(time.time() * 1000),
-        "from": "Producer1"
-    },
-    "body": {
-        "openc2": {
-            "request": {
-                "action": "query",
-                "target": {
-                    "database": {
-                        "database": "system",
-                        "table": "os_version"
-                    }
-                }
-            }
-        }
-    }
-}
-
 query_features = {
     "headers": {
         "request_id": str(uuid.uuid4()),
@@ -42,21 +21,3 @@ query_features = {
     }
 }
 
-
-query_sbom = {
-    "headers": {
-        "request_id": str(uuid.uuid4()),
-        "created": round(time.time() * 1000),
-        "from": "Producer1"
-    },
-    "body": {
-        "openc2": {
-            "request": {
-                "action": "query",
-                "target": {
-                    "sbom": []
-                     }
-            }
-        }
-    }
-}
